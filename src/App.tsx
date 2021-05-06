af0e2f254d6cabc28d5633908ab44e5331b1e608
royeradames <royeraadames@gmail.com>
2021-05-05 21:57:24 -0400

    📦add main content html

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+//assets imports
 import logo from "./assets/logo.svg";
 import leftImg from "./assets/desktop/image-hero-left.png";
 import rightImg from "./assets/desktop/image-hero-right.png";
+
+import womanChatting from "./assets/desktop/image-women-videochatting.jpg";
+import womenInVideoCall from "./assets/desktop/image-woman-in-videocall.jpg";
+import menInMeeting from "./assets/desktop/image-men-in-meeting.jpg";
+import menTexting from "./assets/desktop/image-man-texting.jpg";
+
 function App() {
   return (
-    <main className="main">
+    <>
       <header className="header">
         <img src={logo} alt="Meet logo" />
         <img src={leftImg} alt="" className="header__left-img" />
@@ -22,7 +29,40 @@ function App() {
           <button className="button2 what-is-it">What is it?</button>
         </div>
       </header>
-    </main>
+      <main className="main">
+        <img
+          src={womenInVideoCall}
+          alt="Women in video call"
+          className="main__women-video-call"
+        />
+        <img
+          src={womanChatting}
+          alt="Three woman chatting on the phone"
+          className="main__woman-chatting"
+        />
+        <img
+          src={menInMeeting}
+          alt="Men in a meeting"
+          className="main__men-meeting"
+        />
+        <img
+          src={menTexting}
+          alt="Men texting"
+          className="main__men-texting/image-man-texting.anChatting"
+        />
+        <div className="main__message">
+          <h3 className="main__message-second-title">Built for modern use</h3>
+          <h2 className="main__message-title">
+            Smarter meetings, all in one place
+          </h2>
+          <p className="main__message-content">
+            Send messages, share files, show your screen, and record your
+            meetings — all in one workspace. Control who can join with
+            invite-only team access, data encryption, and data export.
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
 
